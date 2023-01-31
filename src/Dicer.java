ebf5515bbe8d81eee788ddd0e1b863778bb4f29d
Aryan Rai <aryanrai3112@gmail.com>
2023-01-31 23:59:59 +0530

    Commented the code

--- src/Dicer.java
+++ src/Dicer.java
@@ -5,29 +5,33 @@ public class Dicer {
     private static String filePath, fileName, inputImagePath, diceSheetPath;
     private static int sampleRes, diceRes;
 
-    public static void main(String[] args) {
-        initialize();
-        loadDiceSprites();
-        convert();
-    }
-
+    // Function to initialize all the private variables.
     private static void initialize() {
+        /*
+         * These values can be changed based on user preference to manipulate the output image.
+         * diceRes specifies the resolution of each dice face in the dice sheet (Currently only supports 16x16).
+         * sampleRes specifies the resolution of sampling grid that'll be used to plot each dice face.
+         * diceSheetPath specifies the path for the dice sheet image file.
+         * inputImagePath specifies the path for the input image file.
+         */
         diceRes = 16;
         sampleRes = 10;
         diceSheetPath = "res/Dice/DiceSheet.png";
         inputImagePath = "res/Image/6.png";
 
+        // Converting the relative paths to absolute paths.
         diceSheetPath = FileSystems.getDefault().getPath(diceSheetPath).normalize().toAbsolutePath().toString();
         inputImagePath = FileSystems.getDefault().getPath(inputImagePath).normalize().toAbsolutePath().toString();
 
+        // Extracting the file name and file path from the full input image path.
         int pathSeparator = inputImagePath.lastIndexOf('/');
         if (pathSeparator == -1)
             pathSeparator = inputImagePath.lastIndexOf('\\');
-
         filePath = inputImagePath.substring(0, pathSeparator);
         fileName = inputImagePath.substring(pathSeparator + 1, inputImagePath.lastIndexOf('.'));
     }
 
+    // Function to load and extract dice sprites from the dice sheet image file.
     private static void loadDiceSprites() {
         Image diceSheet = new Image(diceSheetPath);
         diceSprites = new Image[6];
@@ -45,32 +49,56 @@ public class Dicer {
         }
     }
 
-    private static void convert() {
+    // Function to create a dice-pattern from the input image and save it to an image file.
+    private static void createDicePattern() {
+        // Load input and output image into memory.
         Image inputImage = new Image(inputImagePath);
         Image outputImage = new Image((inputImage.getWidth() / sampleRes) * diceRes, (inputImage.getHeight() / sampleRes) * diceRes);
+
+        // Calculate the number of dice horizontally and vertically based on the value of sampleRes.
         int diceH = inputImage.getHeight() / sampleRes;
         int diceW = inputImage.getWidth() / sampleRes;
+
+        // Iterate for every dice face in the output image.
         for (int y = 0; y < diceH; y++) {
             for (int x = 0; x < diceW; x++) {
+                // Calculate the bounds of the sample grid that map to each dice face.
                 int yStart = y * sampleRes;
                 int yEnd = yStart + sampleRes;
                 int xStart = x * sampleRes;
                 int xEnd = xStart + sampleRes;
+
+                // Add the color intensity of each pixel in the sample grid.
                 int intensity = 0;
                 for (int yPos = yStart; yPos < yEnd; yPos++) {
                     for (int xPos = xStart; xPos < xEnd; xPos++) {
                         int color = inputImage.getPixel(xPos, yPos);
+                        /*
+                         * Extract the red, green, and blue components from the pixel value and divide by 3
+                         * to find the average pixel intensity. Then divide it by 255 to get normalized pixel intensity
+                         * between 0 and 1. Then multiply it by 6 to find the corresponding dice face for that pixel.
+                         *
+                         * Here division by 3, then by 255, and then multiplication by 6 can be simplified into
+                         * division by 128.
+                         *
+                         * Lastly we are adding up the resulting dice face index for all pixels in the grid to later
+                         * calculate an overall average dice face index for the sample grid.
+                         */
                         int pixIntensity = ((color & 255) + ((color >> 8) & 255) + ((color >> 16) & 255)) / 128;
                         intensity += pixIntensity;
                     }
                 }
-                intensity /= (sampleRes * sampleRes);
-                Image currDice = diceSprites[intensity];
 
+                intensity /= (sampleRes * sampleRes); // Calculate the average intensity to get a normalized contrast value.
+                Image currDice = diceSprites[intensity]; // Get the dice face corresponding to the above contrast value.
+
+                // Recalculate the bounds, but now for the dice face.
                 yStart = y * diceRes;
                 yEnd = yStart + diceRes;
                 xStart = x * diceRes;
                 xEnd = xStart + diceRes;
+
+                // Plot the resulting dice face within calculated bounds in the final output image.
                 for (int yPos = yStart; yPos < yEnd; yPos++) {
                     for (int xPos = xStart; xPos < xEnd; xPos++) {
                         int color = currDice.getPixel(xPos % diceRes, yPos % diceRes);
@@ -79,6 +107,15 @@ public class Dicer {
                 }
             }
         }
-        outputImage.saveImage(filePath, fileName + "_Diced");
+
+        // Save the output image date to an image file in the same directory as the input image file.
+        outputImage.saveToFile(filePath, fileName + "_Diced");
+    }
+
+    // Entry Point.
+    public static void main(String[] args) {
+        initialize();
+        loadDiceSprites();
+        createDicePattern();
     }
 }
--- src/Image.java
+++ src/Image.java
@@ -4,16 +4,17 @@ import java.io.File;
 import java.io.IOException;
 
 public class Image {
-    int width;
-    int height;
-    int[] pixels;
+    private int width, height;
+    private int[] pixelData;
 
-    public Image(int w, int h) {
-        this.width = w;
-        this.height = h;
-        this.pixels = new int[width * height];
+    // Constructor to create an empty Image of specified width and height.
+    public Image(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.pixelData = new int[width * height];
     }
 
+    // Constructor to load Image from the specified image file.
     public Image(String path) {
         if (path == null)
             return;
@@ -25,42 +26,44 @@ public class Image {
             e.printStackTrace();
         }
         assert image != null;
+
         this.width = image.getWidth();
         this.height = image.getHeight();
-        this.pixels = image.getRGB(0, 0, width, height, null, 0, width);
+        this.pixelData = image.getRGB(0, 0, width, height, null, 0, width);
     }
 
-    public void saveImage(String path, String name) {
+    // Function to save the image to a file on specified path.
+    public void saveToFile(String path, String name) {
         File outputFile = new File(path + "/" + name + ".png");
         if (outputFile.exists())
             outputFile.delete();
         try {
-            ImageIO.write(this.getImage(), "png", outputFile);
+            BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+            image.setRGB(0, 0, width, height, pixelData, 0, width);
+            ImageIO.write(image, "png", outputFile);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public BufferedImage getImage() {
-        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-        image.setRGB(0, 0, width, height, pixels, 0, width);
-        return image;
-    }
-
+    // Function to get the color value of the pixel at image coordinate (x, y).
     public final int getPixel(int x, int y) {
         if (x < 0 || x >= width || y < 0 || y >= height) return 0xffff00ff;
-        return pixels[x + y * width];
+        return pixelData[x + y * width];
     }
 
+    // Function to set the color value of the pixel at image coordinate (x, y).
     public final void setPixel(int x, int y, int color) {
         if (x < 0 || x >= width || y < 0 || y >= height) return;
-        pixels[x + y * width] = color;
+        pixelData[x + y * width] = color;
     }
 
+    // Function to get the width of the image.
     public final int getWidth() {
         return width;
     }
 
+    // Function to get the height of the image.
     public final int getHeight() {
         return height;
     }
